d9a7768e0576842a4fa4c25cc3f629d419921cdf
Rodrigo Giannini Costa <62073805+rodrigogianninicosta@users.noreply.github.com>
2024-06-08 17:29:15 -0300

    first commit

---
+++ src/test/java/product/project/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package product.project.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
